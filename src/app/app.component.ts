c3a6748975a48dc1b83dbae7b95ff510e3c00fc5
Schmukas <hlukase@web.de>
2024-12-17 10:47:43 +0100

    import friendlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ShopComponent } from "./shop/shop.component";
+import  {FriendlistComponent } from "./friendlist/friendlist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, ShopComponent, friendlist],
+  imports: [RouterOutlet, RouterLink, ShopComponent, FriendlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
