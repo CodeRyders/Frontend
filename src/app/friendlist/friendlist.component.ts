686126cd521e1f04717c83fbee0b6e9748f5b112
Internet 2 <coderyders@fabianweiss.dev>
2024-12-17 10:53:46 +0100

    added friendlist html and css

--- src/app/friendlist/friendlist.component.ts
+++ src/app/friendlist/friendlist.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from "@angular/common/http"; //Für Daten aus Backend
+
+interface Friend {
+  id: string;
+}
 
 @Component({
   selector: 'app-friendlist',
@@ -7,6 +12,19 @@ import { Component } from '@angular/core';
   templateUrl: './friendlist.component.html',
   styleUrl: './friendlist.component.css'
 })
-export class FriendlistComponent {
+export class FriendlistComponent implements OnInit{
+  friends: Friend[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getFriends();
+  }
+
+  getFriends() {
+    this.http.get<Friend[]>('http://localhost:8080/api/users/knlSulQN5td1nfJ18zAbX3Cg2gk2/friends ')
+      .subscribe(data => {
+        this.friends = data;
+      });
+  }
 }
