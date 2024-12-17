f60723a3b2ab98485a6ff1d53b35030ce69e5886
Fabian-IF <b3-github@fabianweiss.dev>
2024-12-17 14:01:15 +0100

    Added Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ShopComponent } from './shop/shop.component';
 import {LocationsComponent} from "./locations/locations.component";
 import {FriendlistComponent} from "./friendlist/friendlist.component";
+import {HomeComponent} from "./home/home.component";
 
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'locations', component: LocationsComponent },
   { path: 'friends', component: FriendlistComponent },
-  { path: '', redirectTo: '/shop', pathMatch: 'full' }
+  { path: '**', redirectTo: ''}
 ];
